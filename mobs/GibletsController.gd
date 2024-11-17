59b14a7b5eee10a0f67877e2c419509cf5306b92
Zach Helm <zac1790@gmail.com>
2024-11-17 13:45:30 -0500

    Imported UI frames

--- mobs/GibletsController.gd
+++ mobs/GibletsController.gd
@@ -3,11 +3,13 @@ extends Node3D
 var deleteTimer = 10.0
 @onready var bloodSplosion: GPUParticles3D = $BloodSplosion
 @onready var bloodSploosh: GPUParticles3D = $BloodSploosh
+@onready var bonez: GPUParticles3D = $Bonez
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	bloodSplosion.emitting = true
 	bloodSploosh.emitting = true
+	bonez.emitting = true
 	pass # Replace with function body.
 
 
